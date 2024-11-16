fe801410ea54bb4a6cb903400967ab8c8f75adc9
Luke D Iremadze <lukababu@users.noreply.github.com>
2024-11-15 17:16:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*", // Adjust this as needed for security
+        methods: ["GET", "POST"],
+    },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Running");
+});
+
 io.on("connection", (socket) => {
     console.log("id: ", socket.id);
 
@@ -15,9 +33,11 @@ io.on("connection", (socket) => {
         io.to(data.to).emit("callAccepted", data.signal);
     });
 
-    // Add handler for the blackOut event
+    // Add the blackOut event listener
     socket.on("blackOut", ({ targetCaller, isBlackOut }) => {
         console.log(`Received blackOut for ${targetCaller}: ${isBlackOut}`);
         io.to(targetCaller).emit("blackOut", isBlackOut);
     });
 });
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
