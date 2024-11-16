304a69fbd368010cfa25eb65423ddf171b793cb6
Luke D Iremadze <lukababu@users.noreply.github.com>
2024-11-15 17:13:27 -0700

    debug code

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    },
-});
-app.use(cors());
-const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    res.send("Running");
-});
 io.on("connection", (socket) => {
-    socket.emit("me", socket.id);
     console.log("id: ", socket.id);
+
+    socket.emit("me", socket.id);
+
     socket.on("disconnect", () => {
         socket.broadcast.emit("callEnded");
     });
+
     socket.on("callUser", ({ userToCall, signalData, from, name }) => {
         io.to(userToCall).emit("callUser", { signal: signalData, from, name });
     });
+
     socket.on("answerCall", (data) => {
         io.to(data.to).emit("callAccepted", data.signal);
     });
+
+    // Add handler for the blackOut event
+    socket.on("blackOut", ({ targetCaller, isBlackOut }) => {
+        console.log(`Received blackOut for ${targetCaller}: ${isBlackOut}`);
+        io.to(targetCaller).emit("blackOut", isBlackOut);
+    });
 });
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
